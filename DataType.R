9df1b2e0bc51181892abad95ff9c5047f3a8ebe7
susanthrajanala <40766699+susanthrajanala@users.noreply.github.com>
2018-07-06 11:47:22 -0400

    Add files via upload

---
+++ DataType.R
@@ -0,0 +1,63 @@
+songs <- c("Survival", "Nonstop", "Elevate", "Emotionless", "God's Plan", "I'm Upset", "8 Out of 10", "Mob Ties", "Can't Take a Joke", "Sandra's Rose", "Talk Up", "Is There More")
+x <- sample(1:12, 1, replace = TRUE)
+songs[x]
+matricy <- matrix(songs[x], nrow = 1, ncol = 1)
+
+x <- sample(1:12, 1, replace = TRUE)
+songs[x]
+matry <- matrix(songs[x], nrow = 1, ncol = 1)
+matt <- rbind(matricy,matry)
+
+x <- sample(1:12, 1, replace = TRUE)
+songs[x]
+marty <- matrix(songs[x], nrow = 1, ncol = 1)
+matt <- rbind(matt,marty)
+
+x <- sample(1:12, 1, replace = TRUE)
+songs[x]
+hi <- matrix(songs[x], nrow = 1, ncol = 1)
+matt <- rbind(matt,hi)
+
+x <- sample(1:12, 1, replace = TRUE)
+songs[x]
+bye <- matrix(songs[x], nrow = 1, ncol = 1)
+matt <- rbind(matt,bye)
+
+x <- sample(1:12, 1, replace = TRUE)
+songs[x]
+lol <- matrix(songs[x], nrow = 1, ncol = 1)
+matt <- rbind(matt,lol)
+
+x <- sample(1:12, 1, replace = TRUE)
+songs[x]
+hehe <- matrix(songs[x], nrow = 1, ncol = 1)
+matt <- rbind(matt,hehe)
+
+x <- sample(1:12, 1, replace = TRUE)
+songs[x]
+doo <- matrix(songs[x], nrow = 1, ncol = 1)
+matt <- rbind(matt,doo)
+
+x <- sample(1:12, 1, replace = TRUE)
+songs[x]
+moo <- matrix(songs[x], nrow = 1, ncol = 1)
+matt <- rbind(matt,moo)
+
+x <- sample(1:12, 1, replace = TRUE)
+songs[x]
+oink <- matrix(songs[x], nrow = 1, ncol = 1)
+matt <- rbind(matt,oink)
+
+x <- sample(1:12, 1, replace = TRUE)
+songs[x]
+loo <- matrix(songs[x], nrow = 1, ncol = 1)
+matt <- rbind(matt,loo)
+
+x <- sample(1:12, 1, replace = TRUE)
+songs[x]
+noob <- matrix(songs[x], nrow = 1, ncol = 1)
+matt <- rbind(matt,noob)
+
+df <- data.frame(matt)
+View(df)
+
